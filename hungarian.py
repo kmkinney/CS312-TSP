f077e99f1834a10be0d6b5b3ddab06e16702d9a7
kmkinney <65429603+kmkinney@users.noreply.github.com>
2023-01-11 20:19:55 -0700

    Done

--- hungarian.py
+++ hungarian.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 # Using KTHs implementation for this
 def hungarian(graph):
@@ -43,3 +44,78 @@ def hungarian(graph):
     if p[j]:
       ans[p[j] - 1] = j - 1
   return (-v[0], ans)
+
+def get_cycles(match):
+  N = len(match)
+  cycles = []
+  used = set()
+  for i in range(N):
+    if i not in used:
+      used.add(i)
+      c = [i]
+      p = match[i]
+      while p != i:
+        used.add(p)
+        c.append(p)
+        p = match[p]
+      cycles.append(c)
+  cycles.sort(key=len, reverse=True)
+  return cycles
+
+def merge_cycles(adj, match, cs):
+  c1 = cs[0]
+  c2 = cs[1]
+  next = lambda i: match[i]
+  mcost = (
+    lambda i, j: adj[i][match[j]]
+    + adj[j][match[i]]
+    - adj[i][match[i]]
+    - adj[j][match[j]]
+  )
+
+  min_cost = mcost(c1[0], c2[0])
+  min_swap = (c1[0], c2[0])
+  for i in c1:
+    for j in c2:
+      c = mcost(i, j)
+      if c < min_cost:
+        min_cost = c
+        min_swap = (i, j)
+  # print(f'{min_cost=}')
+  print(min_swap)
+  i, j = min_swap
+  # print(f'{min_swap=}')
+  ni = next(i)
+  nj = next(j)
+  match[i] = nj
+  match[j] = ni
+  # print(f'{match=}')
+  return match
+
+if __name__ == '__main__':
+  g = [
+    [np.inf,3,7,8,9],
+    [6,np.inf,2,32,5],
+    [13,1,np.inf,8,12],
+    [1,4,90,np.inf,3],
+    [32,4,32,1,np.inf]
+  ]
+  c, a = hungarian(g)
+  print(c)
+  print(a)
+  print(get_cycles(a))
+  print(merge_cycles(g,a,get_cycles(a)))
+
+  def cost(p):
+    pl = (*p,p[0])
+    c = 0
+    for a,b in itertools.pairwise(pl):
+      c += g[a][b] 
+    return c
+  
+  min_tour = (cost([0,1,2,3,4]),[0,1,2,3,4])
+  for perm in itertools.permutations([0,1,2,3,4]):
+    pc = cost(perm)
+    min_tour = min(min_tour, (pc,perm), key=lambda t: t[0])
+  print(min_tour)
+
